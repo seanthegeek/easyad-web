f80cd2ef5ea7ec71d142fa2705106774918242dd
Sean Whalen <whalenster@gmail.com>
2016-10-06 14:07:32 -0400

    Document the API

--- adapi.py
+++ adapi.py
@@ -1,9 +1,11 @@
+from __future__ import unicode_literals
+
 from os import urandom
 import binascii
 from datetime import datetime
-
-from flask import Flask, jsonify, request
 from functools import wraps
+
+from flask import Flask, jsonify, request, render_template
 from ldap import LDAPError
 from peewee import *
 
@@ -15,8 +17,9 @@ app.config.from_pyfile("config.py")
 ad = EasyAD(app.config)
 db = SqliteDatabase('easyad.db')
 
-def generate_api_key(bytes=16):
-    return str(binascii.hexlify(urandom(bytes)))
+
+def generate_api_key(byte_size=16):
+    return binascii.hexlify(urandom(byte_size)).decode(encoding="utf-8")
 
 
 class BaseModel(Model):
@@ -27,9 +30,9 @@ class BaseModel(Model):
 class APIUser(BaseModel):
     name = CharField(unique=True)
     api_key = CharField(default=generate_api_key, unique=True)
-    enabled = BooleanField()
-    created = DateTimeField(datetime.now)
-    updated = DateTimeField()
+    enabled = BooleanField(default=True)
+    created = DateTimeField(default=datetime.now)
+    updated = DateTimeField(default=datetime.now)
 
     def enable(self):
         self.enabled = True
@@ -58,6 +61,8 @@ class APIAuditRecord(BaseModel):
     timestamp = DateTimeField(default=datetime.now)
     action = CharField()
 
+db.create_tables([APIUser, APIAuditRecord], safe=True)
+
 
 def parse_ldap_error(e):
     return "An LDAP error occurred - {0}".format(e.args[0]["desc"])
@@ -66,9 +71,17 @@ def parse_ldap_error(e):
 def api_call(function):
     @wraps(function)
     def process_api_call(*args, **kwargs):
+        if "api_key" not in request.args:
+            return jsonify(dict(error="You must supply an api_key parameter")), 401
         try:
-           return function(*args, **kwargs)
+            print(request.args["api_key"])
+            user = APIUser.get(api_key=request.args["api_key"])
+            if not user.enabled:
+                return jsonify(dict(error="This API key is disabled")), 403
+            return function(*args, **kwargs)
 
+        except DoesNotExist:
+            return jsonify(dict(error="The API key is invalid")), 403
         except ValueError as e:
             return jsonify(dict(error=str(e))), 404
         except LDAPError as e:
@@ -79,68 +92,107 @@ def api_call(function):
 
 @app.route("/")
 def index():
-    return "Hello world!"
+    return render_template("index.html")
+
+
+@app.route("/authenticate", methods=["GET", "POST"])
+@api_call
+def authenticate_user():
+    if request.method != "POST":
+        return jsonify(dict(error="Must be sent as a POST request")), 400
+    if "username" not in request.args or "password" not in request.args:
+        return jsonify(dict(error="Must provide a username and password")), 400
+    return ad.authenticate_user(request.args["username"], request.args["password"])
 
 
 @app.route("/user/<user_string>")
 @api_call
 def get_user(user_string):
-    return jsonify(ad.get_user(user_string, json_safe=True))
+    base = None
+    if "base" in request.args:
+        base = request.args["base"]
+    attributes = None
+    if "attributes" in request.args:
+        attributes = request.args["attributes"].split(",")
+    return jsonify(ad.get_user(user_string, base=base, attributes=attributes, json_safe=True))
 
 
 @app.route("/user/<user_string>/groups")
 @api_call
 def user_groups(user_string):
-    return jsonify(ad.get_all_user_groups(user_string, json_safe=True))
+    base =None
+    if "base" in request.args:
+        base = request.args["base"]
+    return jsonify(ad.get_all_user_groups(user_string, base=base, json_safe=True))
 
 
 @app.route("/user/<user_string>/member-of/<group_string>")
 @api_call
 def user_is_member_of_group(user_string, group_string):
-    return jsonify(dict(member=ad.user_is_member_of_group(user_string, group_string)))
+    base = None
+    if "base" in request.args:
+        base = request.args["base"]
+    return jsonify(dict(member=ad.user_is_member_of_group(user_string, group_string, base=base)))
 
 
 @app.route("/group/<group_string>")
 @api_call
 def get_group(group_string):
-    return jsonify(ad.get_group(group_string, json_safe=True))
+    base = None
+    if "base" in request.args:
+        base = request.args["base"]
+    attributes = None
+    if "attributes" in request.args:
+        attributes = request.args["attributes"].split(",")
+    return jsonify(ad.get_group(group_string, base=base, attributes=attributes, json_safe=True))
 
 
-@app.route("/group/<group_string>/members")
+@app.route("/group/<group_string>/users")
 @api_call
 def get_group_members(group_string):
-    return jsonify(ad.get_all_users_in_group(group_string, json_safe=True))
+    base = None
+    if "base" in request.args:
+        base = request.args["base"]
+    return jsonify(ad.get_all_users_in_group(group_string, base=base, json_safe=True))
 
 
-@app.route("/search/user/<user_string>")
+@app.route("/search/users/<user_string>")
 @api_call
 def search_for_users(user_string):
     search_attributes = None
     return_attributes = None
+    base = None
 
+    if "base" in request.args:
+        base = request.args["base"]
     if "search_attributes" in request.args:
         search_attributes = request.args["search_attributes"].split(",")
     if "return_attributes" in request.args:
         return_attributes = request.args["return_attributes"].split(",")
 
     return jsonify(ad.search_for_users(user_string,
+                                       base=base,
                                        search_attributes=search_attributes,
                                        return_attributes=return_attributes,
                                        json_safe=True))
 
 
-@app.route("/search/group/<group_string>")
+@app.route("/search/groups/<group_string>")
 @api_call
 def search_for_groups(group_string):
     search_attributes = None
     return_attributes = None
+    base = None
 
+    if "base" in request.args:
+        base = request.args["base"]
     if "search_attributes" in request.args:
         search_attributes = request.args["search_attributes"].split(",")
     if "return_attributes" in request.args:
         return_attributes = request.args["return_attributes"].split(",")
 
     return jsonify(ad.search_for_groups(group_string,
+                                        base=base,
                                         search_attributes=search_attributes,
                                         return_attributes=return_attributes,
                                         json_safe=True))
--- manage.py
+++ manage.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
+from __future__ import print_function, unicode_literals
+
+from functools import wraps
+from sys import stderr
+
 from flask_script import Manager
+from peewee import DoesNotExist, IntegrityError
 
 from adapi import app, db, APIUser, APIAuditRecord
 
@@ -9,18 +15,96 @@ manager = Manager(app)
 db.create_tables([APIUser, APIAuditRecord], safe=True)
 
 
-@manager.option("-c --create", dest="username")
+def _get_api_user(user_string):
+    """
+    Gets an APIUser or prints an error and exits
+    Args:
+        user_string: the user's name or API key
+
+    Returns:
+        The user APIUser object
+
+    """
+    try:
+        user_string = user_string.lower()
+        user = APIUser.select().where((APIUser.name == user_string) |
+                                      (APIUser.api_key == user_string)).get()
+        return user
+
+    except DoesNotExist:
+        print("Error: {0} does not exist".format(user_string), file=stderr)
+        exit(-1)
+
+
+@manager.command
 def create_api_user(username):
-    user = APIAuditRecord.create(name=username)
-    print(user.name)
-    print(user.api_key)
+    """Create an API user"""
+    try:
+        user = APIUser.create(name=username.lower())
+        print(user.name)
+        print(user.api_key)
+
+    except IntegrityError:
+        print("Error: {0} already exists".format(username), file=stderr)
+        exit(-1)
 
 
-@manager.option("-d --disable", dest="username")
+@manager.command
 def disable_api_user(username):
-    return APIAuditRecord.create(name=username)
+    """Disable access for an API user"""
+    user = _get_api_user(username)
+    user.enabled = False
+    user.save()
+
+    print("{0} disabled".format(username))
 
 
-@manager.option("-e --enable", dest="username")
-def enaable_api_user(username):
-    return APIAuditRecord.create(name=username)
+@manager.command
+def enable_api_user(username):
+    """Enable access for an API user"""
+    user = _get_api_user(username)
+    user.enabled = True
+    user.save()
+
+    print("{0} enabled".format(username))
+
+
+@manager.command
+def rename_api_user(current_name, new_name):
+    """Rename an API user"""
+    user = _get_api_user(current_name)
+    user.name = new_name
+    user.save()
+    print("{0} is now {1}".format(current_name, new_name))
+
+
+@manager.command
+def reset_api_user(username):
+    """Generate a new API key for an API user"""
+    user = _get_api_user(username)
+    api_key = user.reset()
+    print(username)
+    print(api_key)
+
+
+@manager.command
+def list_api_users():
+    """List the names of all API users"""
+    users = APIUser.select()
+    for user in users:
+        print(user.name)
+
+
+@manager.command
+def show_api_user(lookup_string):
+    """Lookup an API user by name or API key"""
+    user = _get_api_user(lookup_string)
+    print("Username: {0}".format(user.name))
+    print("API Key: {0}".format(user.api_key))
+    print("Enabled: {0}".format(user.enabled))
+    print("Created: {0}".format(user.created.strftime("%x %X UTC")))
+    print("Updated: {0}".format(user.updated.strftime("%x %X UTC")))
+
+
+if __name__ == "__main__":
+    manager.run()
